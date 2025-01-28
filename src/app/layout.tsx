e279bf307c00c25ae8ba06af33e0321e2591da75
Julian Haeger <julian.haeger@evergreensmartpower.co.uk>
2025-01-28 07:35:15 +0000

    Fix non-dynamic generateMetadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { Metadata } from 'next'
 import { PropsWithChildren } from "react"
 
 import '@/styles/globals.css'
 import '@/styles/typography2.css'
 import styles from '@/styles/Layout.module.css'
 
-export async function generateMetadata () {
-  return {
-    title: {
-      template: '%s | Julian Haeger',
-      default: 'Home'
-    },
-    viewport: "width=device-width, initial-scale=1"
-  }
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Julian Haeger',
+    default: 'Home'
+  },
+  viewport: "width=device-width, initial-scale=1"
 }
 
+
 function RootLayout({children}: PropsWithChildren<{}>){
-return (
-  <html lang="en">
-    <body>
-      <header className="page-header">
-        <div>
-          <div className={styles.banner}>
-            <h1><Link href="/">Julian&apos;s Thoughts</Link></h1>
+  return (
+    <html lang="en">
+      <body>
+        <header className="page-header">
+          <div>
+            <div className={styles.banner}>
+              <h1><Link href="/">Julian&apos;s Thoughts</Link></h1>
+            </div>
+            {/* <nav>This is nav</nav> */}
           </div>
-          {/* <nav>This is nav</nav> */}
-        </div>
-      </header>
-      {children}
-    </body>
-  </html>)
-}
+        </header>
+        {children}
+      </body>
+    </html>)
+  }
 
 export default RootLayout
